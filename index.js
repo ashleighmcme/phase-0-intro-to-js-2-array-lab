f9b309edbb3cf70793beb752241df7408529cde7
Ashleigh McMahon <ashleighmc@me.com>
2023-08-02 17:07:50 -0600

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution here!
+const cats=["Milo","Otis","Garfield"];
+
+
+function destructivelyAppendCat(name){
+cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function appendCat(name){
+    const copyOfCats=[...cats]
+    copyOfCats.push(name)
+return copyOfCats    
+}
+
+console.log(appendCat("Patches"))
+console.log(cats)
+
+function prependCat(name){
+    const copyOfCats=[...cats]
+    copyOfCats.unshift(name)
+    
+return copyOfCats
+}
+
+
+function removeLastCat(){
+    const copyOfCats=[...cats]
+    copyOfCats.pop()
+    return copyOfCats
+
+}
+
+function removeFirstCat(){
+    const copyOfCats=[...cats]
+    copyOfCats.shift()
+    return copyOfCats
+}
+
+
